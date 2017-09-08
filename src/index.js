5a50c014383fb490d9bb6b949e36122c4a704383
MarleyPlant <marley@marleyplant.com>
2017-09-08 16:35:31 +0100

    Add command prefixs

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ var config = require('./config')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(config.playing)
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if ( msg.content.split(config.prefix)[1].split(" ")[0]) {
+    if (msg.content === '!ping') {
+      msg.reply('Pong!');
+    }
   }
 });
 
