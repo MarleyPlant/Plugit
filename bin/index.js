7be89030c8e0f34e4faf764de867791b4d09f6d2
MarleyPlant <marley@marleyplant.com>
2017-09-08 19:22:41 +0100

    update folder structure and add support for loading commands via npm modules

---
+++ bin/index.js
@@ -0,0 +1,107 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const glob = require("glob");
+const archy = require("archy");
+var log = require('../lib/log');
+var path = require('path');
+var chalk = require('chalk');
+var config = require('./config');
+var commands = {}; //Create Dictionary to store Commands
+
+function getModulesPaths () {
+  if (process.env.NODE_ENV === 'test') {
+    return [path.join(__dirname, '..', 'test')];
+  }
+  var sep = (process.platform === 'win32') ? ';' : ':';
+  var paths = [];
+
+  if (process.env.NODE_PATH) {
+    paths = paths.concat(process.env.NODE_PATH.split(sep));
+  } else {
+    if (process.platform === 'win32') {
+      paths.push(path.join(process.env.APPDATA, 'npm', 'node_modules'));
+    } else {
+      paths.push('/usr/lib/node_modules');
+    }
+  }
+  return paths;
+}
+
+function findModules (searchpaths) {
+  return searchpaths.reduce(function (arr, searchpath) {
+    return arr.concat(glob.sync('{@*/,}plugit-*', {cwd: searchpath, stat: true}).map(function (match) {
+      var Module = {path: path.join(searchpath, match), name: match.replace(/(?:@[\w]+[\/|\\]+)?plugit-/, ""), pkg: {}};
+      try {
+        Module.pkg = require(path.join(searchpath, match, 'package.json'));
+      } catch (e) {
+      }
+      return Module;
+    }));
+  }, []);
+}
+
+function getAllModules () {
+  return findModules(getModulesPaths());
+}
+
+function logModules(Modules) {
+  var tree = {
+    label: 'Installed Modules',
+    nodes: Modules.map(function (gen) {
+      return {label: gen.name + (gen.pkg.version ? chalk.grey(' (' + gen.pkg.version + ')') : '')};
+    })
+  };
+  archy(tree).split('\n').forEach(function(v) {
+    if (v.trim().length === 0) return;
+    log(v);
+  });
+}
+
+var loadCommands = function() {
+    var modules = getAllModules();
+    for (let module of modules) {
+      commands[module.name] = require(module.path + "/" + module.pkg.main);
+      if(config.debug) {
+        console.log("Loaded " + module.name);
+      }
+    }
+
+    var files = fs.readdirSync("../modules/");
+    for (let file of files) {
+        if (file.endsWith('.js')) {
+          commands[file.slice(0, -3)] = require("../modules/" + file);
+  			  if(config.debug) {
+              console.log("Loaded " + file);
+          }
+    }
+  }
+  console.log("———— Commands Loaded! ————");
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Installed NPM Modules: ');
+  logModules(getAllModules());
+  client.user.setGame(config.playing)
+  loadCommands();
+});
+
+client.on('message', msg => {
+  if ( msg.content.split(config.prefix)[1].split(" ")[0] ) {
+    var command = msg.content.split("!")[1];
+    if(command in commands){
+      if(config.delete_commands){
+         msg.delete();
+      }
+      commands[command].main(client, msg);
+    }
+    else if ( command in modules) {
+      console.log(command);
+      console.log(modules[command]);
+      modules[command].pkg.main(client, msg);
+    }
+  }
+});
+
+client.login(config.token);
--- modules/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    main: function(bot, msg) {
-      msg.channel.send("Pong!")
-    },
-    help: 'Ping the bot'
-};
