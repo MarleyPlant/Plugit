06a0a9d9d4b63a3042fbf7d60853a4daa87c4a7c
MarleyPlant <marley@marleyplant.com>
2017-12-03 18:24:56 +0000

    Add basic handlebars rendering

--- express/express.js
+++ express/express.js
@@ -1,12 +1,16 @@
 var express = require('express');
-    http = require("http");
+    exphbs = require("express-handlebars");
+
 var app = express();
 
 
 app.set('port', (process.env.PORT || 5000));
-
+app.engine('handlebars', exphbs());
+app.set('view engine', "handlebars");
 app.use(express.static(__dirname + '/www'));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port Number: " + app.get('port'));
+app.get('/', function(req, res) {
+  res.render('login', {layout: false});
 });
+
+app.listen(app.get('port'))
