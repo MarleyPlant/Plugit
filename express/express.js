c3ff7bc6930bd96c7b82c78957b3764f5f3954dd
MarleyPlant <marley@marleyplant.com>
2017-12-03 18:09:56 +0000

    Updated express server

--- express/express.js
+++ express/express.js
@@ -1,5 +1,12 @@
 var express = require('express');
+    http = require("http");
+var app = express();
 
-app.set('port', (process.env.port || 5000));
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/www'));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port Number: " + app.get('port'));
+});
