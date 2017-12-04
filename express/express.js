517b1b16d5fd5e268ad3985e4d6cc7f551eb6d79
Frixoe <suryansh5060@gmail.com>
2017-12-04 20:05:03 +0900

    Improve and fix issues with handlebars and move 'views' outside 'express.'

--- express/express.js
+++ express/express.js
@@ -1,16 +1,21 @@
 var express = require('express');
-    exphbs = require("express-handlebars");
-
 var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
 app.set('port', (process.env.PORT || 5000));
-app.engine('handlebars', exphbs());
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', "handlebars");
 app.use(express.static(__dirname + '/www'));
 
 app.get('/', function(req, res) {
-  res.render('login', {layout: false});
+  res.render('login', {title: 'Login'});
 });
 
-app.listen(app.get('port'))
+app.get('/404', function(req, res){
+  res.render('404', {title: 'Crazy Shit!'});
+});
+
+app.listen(app.get('port'));
+
+console.log('Now listening on ' + app.get('port')); // To know when the server starts.
