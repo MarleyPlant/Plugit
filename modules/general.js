aa96c93b90a939768ed8cc8cb3402c06fecc2535
Marley Plant <marley@marleyplant.com>
2020-07-01 21:12:10 +0000

    Fix Broken Legacy Code For Importing Modules

---
+++ modules/general.js
@@ -0,0 +1,52 @@
+const util = require("..//util");
+
+module.exports = {
+  commands: {
+    ping: {
+      name: "ping",
+      help: 'Ping the bot',
+      main: function(bot, db, msg) {
+        msg.channel.send(`${bot.ping}ms pong!`)
+      },
+    },
+
+    clear: {
+      name: "clear",
+      parameters: "(number)",
+      help: "clear messages from the chat.",
+      main: function(bot, db, msg) {
+        args = util.args.parse(msg);
+        msg.channel.bulkDelete(args[0]);
+      }
+    },
+
+    stats: {
+      name: "stats",
+      help: "Display statistics for the bot instance." ,
+      main: function(bot, db, msg) {
+        msg.channel.send(`Connected to ${bot.guilds.size} guilds, serving ${bot.users.size} users.`)
+      }
+    },
+
+    softban: {
+      name: "softban",
+      help: "Ban then unban a user to delete messages",
+      parameters: "(user)",
+      main: function(bot, db, msg) {
+        mentioned = msg.mentions.members.first()
+        if (msg.member.hasPermission("BAN_MEMBERS")) {
+          msg.guild.ban(mentioned.id)
+          msg.guild.unban(mentioned.id)
+
+          msg.react("👍")
+        }
+        else {
+          util.notifications.warn(msg, msg.author + " Attempted to softban user " + mentioned)
+        }
+      }
+    }
+  },
+
+  events: {
+  }
+};
---
+++ util/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  args: require('./args'),
+  pluginManager: require('./PluginManager'),
+  notifications: require('./notifications')
+}
