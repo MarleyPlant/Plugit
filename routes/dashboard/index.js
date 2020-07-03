a35210a1a1df357c8ad3993757a4d9ec8fab3b00
Marley Plant <marley@marleyplant.com>
2020-07-04 00:57:22 +0100

    Conditional Server Rendering and Add Dev Seed for DB

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -6,48 +6,23 @@ const knex = require('knex')(require('../../knexfile').development);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let servers = [
-    {
-      title: 'Server 1',
-      id: 1,
-      discord_ID: 325252,
-      description: 'This is an example server to be connected to the database',
-    },
-    {
-      title: 'Server 2',
-      id: 2,
-      discord_ID: 325236,
-      description: 'This is an example server to be connected to the database',
-    },
-    {
-      title: 'Server 3',
-      id: 3,
-      discord_ID: "702971988351647907",
-      description: 'This is an example server to be connected to the database',
-    },
-    {
-      title: 'Server 4',
-      id: 4,
-      discord_ID: "363250326603563009",
-      description: 'This is an example server to be connected to the database',
-    },
-  ];
-
-  knex('users').where({id: 5}).first()
-  .then((user) => { 
-    servers_to_render = [];
-    for (let server of servers) {
-      console.log(server.discord_ID)
-      if (user.guilds.includes(server.discord_ID)) {
-        servers_to_render.push(server);
+  knex('servers')
+  .then((servers) => {
+    knex('users').where({id: 1}).first()
+    .then((user) => { 
+      servers_to_render = [];
+      guilds = JSON.parse(user.guilds);
+      for (let server of servers) {
+        if (guilds.includes(server.discord_ID)) {
+          servers_to_render.push(server);
+        }
       }
-    }
+      res.render('dashboard', { servers: servers_to_render });
+     })
+    .catch((err) => { console.log(err) });
+  
    })
   .catch((err) => { console.log(err) });
-  
-
-  console.log(servers_to_render);
-  res.render('dashboard', { servers: servers_to_render });
 });
 
 router.use('/server', serverRouter);
