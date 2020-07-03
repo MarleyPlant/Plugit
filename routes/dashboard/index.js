ee546a3490254193b7844c3db59644b468088b9d
Marley Plant <marley@marleyplant.com>
2020-07-04 00:11:19 +0100

    Add Fontawesome, Huge UI Improvements.

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -5,7 +5,34 @@ var settingsRouter = require('./settings');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard');
+  let servers = [
+    {
+      title: 'Server 1',
+      id: 1,
+      discord_ID: 325252,
+      description: 'This is an example server to be connected to the database',
+    },
+    {
+      title: 'Server 2',
+      id: 2,
+      discord_ID: 325236,
+      description: 'This is an example server to be connected to the database',
+    },
+    {
+      title: 'Server 3',
+      id: 3,
+      discord_ID: 23759,
+      description: 'This is an example server to be connected to the database',
+    },
+    {
+      title: 'Server 4',
+      id: 4,
+      discord_ID: 23759,
+      description: 'This is an example server to be connected to the database',
+    },
+  ];
+  
+  res.render('dashboard', { servers: servers });
 });
 
 router.use('/server', serverRouter);
