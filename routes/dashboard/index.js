9fd9f57f8d1880c16505ebbeda6144264dd5715d
Marley Plant <marley@marleyplant.com>
2020-07-04 00:27:53 +0100

    Add Conditional Rendering To Server Cards based on database.

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var serverRouter = require('./server');
 var settingsRouter = require('./settings');
+const knex = require('knex')(require('../../knexfile').development);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -21,18 +22,32 @@ router.get('/', function(req, res, next) {
     {
       title: 'Server 3',
       id: 3,
-      discord_ID: 23759,
+      discord_ID: "702971988351647907",
       description: 'This is an example server to be connected to the database',
     },
     {
       title: 'Server 4',
       id: 4,
-      discord_ID: 23759,
+      discord_ID: "363250326603563009",
       description: 'This is an example server to be connected to the database',
     },
   ];
+
+  knex('users').where({id: 5}).first()
+  .then((user) => { 
+    servers_to_render = [];
+    for (let server of servers) {
+      console.log(server.discord_ID)
+      if (user.guilds.includes(server.discord_ID)) {
+        servers_to_render.push(server);
+      }
+    }
+   })
+  .catch((err) => { console.log(err) });
   
-  res.render('dashboard', { servers: servers });
+
+  console.log(servers_to_render);
+  res.render('dashboard', { servers: servers_to_render });
 });
 
 router.use('/server', serverRouter);
