142b3033d83a811185c3b83a48048aa944d74d08
Marley Plant <marley@marleyplant.com>
2020-07-08 11:59:07 +0100

    Render settings from array

--- routes/settings.js
+++ routes/settings.js
@@ -6,7 +6,26 @@ var isAuth = require('../helpers/isAuth');
 
 router.get('/', function(req, res, next) {
 res.render('settings', {
-    title: 'Plugit - Settings'
+    title: 'Plugit - Settings',
+    settings: [
+        {
+            id: 1
+            icon: "cog",
+            name: 'General Settings',
+            active: true 
+        },
+        {
+            id: 2
+            icon: 'globe',
+            name: 'Connection Settings'
+        },
+        {
+            id: 3
+            icon: 'plug',
+            name: 'Plugin Manager'
+        },
+    ],
+    tabid: 1
 });
 });
 
