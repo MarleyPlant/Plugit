d9a731b283c1b15ff1714433f624c6c69d6f8550
Marley Plant <marley@marleyplant.com>
2020-07-10 03:28:03 +0100

    Update General Commands

--- modules/general.js
+++ modules/general.js
@@ -1,22 +1,18 @@
-const util = require("..//util");
+const util = require("../util");
 
 module.exports = {
   commands: {
-    ping: {
-      name: "ping",
-      help: 'Ping the bot',
-      main: function(bot, db, msg) {
-        msg.channel.send(`${bot.ping}ms pong!`)
-      },
-    },
-
     clear: {
       name: "clear",
       parameters: "(number)",
       help: "clear messages from the chat.",
       main: function(bot, db, msg) {
         args = util.args.parse(msg);
-        msg.channel.bulkDelete(args[0]);
+        if(args[0] == null) {
+          msg.channel.bulkDelete(10);
+        } else {
+          msg.channel.bulkDelete(args[0]);
+        }
       }
     },
 
@@ -24,25 +20,18 @@ module.exports = {
       name: "stats",
       help: "Display statistics for the bot instance." ,
       main: function(bot, db, msg) {
-        msg.channel.send(`Connected to ${bot.guilds.size} guilds, serving ${bot.users.size} users.`)
+        guilds = bot.guilds.cache.array()
+
+        msg.channel.send(`I am currently connected to and serving ${guilds.length} guilds`)
       }
     },
 
     softban: {
-      name: "softban",
-      help: "Ban then unban a user to delete messages",
+      name: "!banned",
+      help: "Displays A List Of Banned Users.",
       parameters: "(user)",
       main: function(bot, db, msg) {
-        mentioned = msg.mentions.members.first()
-        if (msg.member.hasPermission("BAN_MEMBERS")) {
-          msg.guild.ban(mentioned.id)
-          msg.guild.unban(mentioned.id)
 
-          msg.react("👍")
-        }
-        else {
-          util.notifications.warn(msg, msg.author + " Attempted to softban user " + mentioned)
-        }
       }
     }
   },
